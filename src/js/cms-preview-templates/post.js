8eec87f7c123ec47d974e3fc90864b75cafcc30d
IliassTAHIRI <iliass.tahiri@gmail.com>
2018-08-01 17:56:33 +0200

    c

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -7,7 +7,7 @@ export default class PostPreview extends React.Component {
 
     return <div className="w-100 center ph3 pv4">
       <h1 className="f2 lh-title b mt0 mb3">{ entry.getIn(["data", "title"])}</h1>
-      <div className="flex justify-between grey-3">
+      <div className="w-100 flex justify-between grey-3">
         <div style={{
           width: "80px",
           height: "80px"
@@ -15,7 +15,7 @@ export default class PostPreview extends React.Component {
         <p className="mt0">{ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }</p>
         <p className="mt0">Read in x minutes</p>
       </div>
-      <div className="cms w-100">
+      <div className="w-100">
         { widgetFor("body") }
       </div>
     </div>;
